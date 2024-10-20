201163de54bae37cecb7a49eb95ea49ed44205b1
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-10-19 21:31:08 -0600

    recuperando Factura pt2

--- Tienda_de_Peliculas/frmFactura.Designer.cs
+++ Tienda_de_Peliculas/frmFactura.Designer.cs
@@ -28,12 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.btnVentaAlquiler = new System.Windows.Forms.Button();
+            this.btnRegistroEmpleados = new System.Windows.Forms.Button();
+            this.btnRegistroClientes = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.btnReportes);
+            this.panelMenu.Controls.Add(this.btnInventario);
+            this.panelMenu.Controls.Add(this.btnVentaAlquiler);
+            this.panelMenu.Controls.Add(this.btnRegistroEmpleados);
+            this.panelMenu.Controls.Add(this.btnRegistroClientes);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 461);
+            this.panelMenu.TabIndex = 13;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnReportes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.Location = new System.Drawing.Point(0, 315);
+            this.btnReportes.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Padding = new System.Windows.Forms.Padding(11, 2, 0, 0);
+            this.btnReportes.Size = new System.Drawing.Size(220, 60);
+            this.btnReportes.TabIndex = 5;
+            this.btnReportes.Text = "    Reportes";
+            this.btnReportes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReportes.UseVisualStyleBackColor = true;
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnInventario.FlatAppearance.BorderSize = 0;
+            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventario.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnInventario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInventario.Location = new System.Drawing.Point(0, 255);
+            this.btnInventario.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Padding = new System.Windows.Forms.Padding(11, 2, 0, 0);
+            this.btnInventario.Size = new System.Drawing.Size(220, 60);
+            this.btnInventario.TabIndex = 4;
+            this.btnInventario.Text = "    Inventario";
+            this.btnInventario.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInventario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInventario.UseVisualStyleBackColor = true;
+            // 
+            // btnVentaAlquiler
+            // 
+            this.btnVentaAlquiler.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnVentaAlquiler.FlatAppearance.BorderSize = 0;
+            this.btnVentaAlquiler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVentaAlquiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVentaAlquiler.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnVentaAlquiler.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVentaAlquiler.Location = new System.Drawing.Point(0, 195);
+            this.btnVentaAlquiler.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.btnVentaAlquiler.Name = "btnVentaAlquiler";
+            this.btnVentaAlquiler.Padding = new System.Windows.Forms.Padding(11, 2, 0, 0);
+            this.btnVentaAlquiler.Size = new System.Drawing.Size(220, 60);
+            this.btnVentaAlquiler.TabIndex = 3;
+            this.btnVentaAlquiler.Text = "    Venta / Alquiler";
+            this.btnVentaAlquiler.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVentaAlquiler.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnVentaAlquiler.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistroEmpleados
+            // 
+            this.btnRegistroEmpleados.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRegistroEmpleados.FlatAppearance.BorderSize = 0;
+            this.btnRegistroEmpleados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistroEmpleados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistroEmpleados.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnRegistroEmpleados.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistroEmpleados.Location = new System.Drawing.Point(0, 135);
+            this.btnRegistroEmpleados.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.btnRegistroEmpleados.Name = "btnRegistroEmpleados";
+            this.btnRegistroEmpleados.Padding = new System.Windows.Forms.Padding(11, 2, 0, 0);
+            this.btnRegistroEmpleados.Size = new System.Drawing.Size(220, 60);
+            this.btnRegistroEmpleados.TabIndex = 2;
+            this.btnRegistroEmpleados.Text = "    Empleados\r\n";
+            this.btnRegistroEmpleados.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistroEmpleados.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegistroEmpleados.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistroClientes
+            // 
+            this.btnRegistroClientes.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnRegistroClientes.FlatAppearance.BorderSize = 0;
+            this.btnRegistroClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistroClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistroClientes.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnRegistroClientes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistroClientes.Location = new System.Drawing.Point(0, 75);
+            this.btnRegistroClientes.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+            this.btnRegistroClientes.Name = "btnRegistroClientes";
+            this.btnRegistroClientes.Padding = new System.Windows.Forms.Padding(11, 2, 0, 0);
+            this.btnRegistroClientes.Size = new System.Drawing.Size(220, 60);
+            this.btnRegistroClientes.TabIndex = 1;
+            this.btnRegistroClientes.Text = "    Clientes";
+            this.btnRegistroClientes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistroClientes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegistroClientes.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelLogo.Controls.Add(this.label3);
+            this.panelLogo.Controls.Add(this.label2);
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 75);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Suissnord", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 30);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PELICULAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Suissnord", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "TIENDA DE ";
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelTitleBar.Location = new System.Drawing.Point(218, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(767, 75);
+            this.panelTitleBar.TabIndex = 12;
+            // 
+            // frmFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.panelTitleBar);
+            this.Name = "frmFactura";
             this.Text = "frmFactura";
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnInventario;
+        private System.Windows.Forms.Button btnVentaAlquiler;
+        private System.Windows.Forms.Button btnRegistroEmpleados;
+        private System.Windows.Forms.Button btnRegistroClientes;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelTitleBar;
     }
 }
