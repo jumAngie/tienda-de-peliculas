e6887199788f6dd0a935af55c0153afca72ca9b6
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-10-19 21:48:11 -0600

    Revert "Merge branch 'Angie-Diseño-Formulario-Inicio' into Nicole"
    This reverts commit b0945d7c264ec4e653b43dfa91444b0d9caeb2b7, reversing
    changes made to 05b5b7f319ba191ccba7b664105a94799e15078d.

--- Tienda_de_Peliculas/frmPrincipal.cs
+++ Tienda_de_Peliculas/frmPrincipal.cs
@@ -18,68 +18,6 @@ namespace Tienda_de_Peliculas
             tHora.Enabled = true;
         }
 
-        //
-        private Color SelectThemeColor()
-        {
-            int index = random.Next(ThemeColor.ColorList.Count);
-            while(tempIndex == index) 
-            {
-               index = random.Next(ThemeColor.ColorList.Count);
-            }
-            tempIndex = index;
-            string color = ThemeColor.ColorList[index];
-            return ColorTranslator.FromHtml(color);
-        }
-
-        private void ActivateButton(object btnSender)
-        {
-            if(btnSender != null)
-            {
-                if (currentButton != (Button)btnSender) { 
-                
-                    DisableButton();
-                    Color color = SelectThemeColor();
-                    currentButton= (Button)btnSender;
-                    currentButton.BackColor = color;
-                    currentButton.ForeColor = Color.White;
-                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                    panelTitleBar.BackColor = color;
-                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
-                }
-            }
-        }
-
-        private void DisableButton()
-        {
-            foreach (Control previousBtn in panelMenu.Controls) 
-            { 
-                if(previousBtn.GetType() == typeof(Button))
-                    {
-                        previousBtn.BackColor = Color.FromArgb(51, 51, 76);
-                        previousBtn.ForeColor = Color.Gainsboro;
-                        previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                }
-            }
-
-        }
-
-        #region Dashboard 
-        public void mostrarDatosDashboard()
-        { 
-            DateTime fechaActual = DateTime.Now;
-            var diaSemana = (int)DateTime.Now.DayOfWeek;
-            DateTime fechaLunes = fechaActual.AddDays(-((diaSemana == 0 ? 7 : diaSemana) - 1));
-            DateTime fechaDomingo = fechaLunes.AddDays(6);
-
-
-            lblVentasSemanales.Text =      FacturaDAL.CantidadVentasSemanaActual(fechaLunes, fechaDomingo);
-            lblClientesRegistrados.Text = ClientesDAL.CantidadDeClientesRegistrdos();
-            lblAlquileresSemanales.Text = FacturaDAL.CantidadAlquileresSemanaActual(fechaLunes, fechaDomingo);
-            
-            
-        }
-        #endregion
-
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             lblFecha.Text = DateTime.Now.ToLongDateString();
