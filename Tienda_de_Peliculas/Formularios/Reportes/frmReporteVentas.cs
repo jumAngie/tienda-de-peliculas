34c443e4feb08012d4ef308075ed93fd1ac85cfc
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-10-24 22:07:55 -0600

    terminé 1 reporteeeeeeeee

--- Tienda_de_Peliculas/Formularios/Reportes/frmReporteVentas.cs
+++ Tienda_de_Peliculas/Formularios/Reportes/frmReporteVentas.cs
@@ -88,10 +88,61 @@ namespace Tienda_de_Peliculas.Reportes
         private void btnImprimir_Click(object sender, EventArgs e)
         {
             SaveFileDialog savefile = new SaveFileDialog();
-            savefile.FileName = "REPORTE VENTAS POR PELICULAS - " + string.Format("{0}.pdf", DateTime.Now.ToString("ddMMyyyy"));
-            
+            savefile.FileName = "REPORTE VENTAS POR PELICULAS - " + string.Format("{0}.pdf", DateTime.Now.ToString("dd/MM/yyyy"));
 
-           
+            string PaginaHTML_Texto = Properties.Resources.Plantilla.ToString();
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@TIPOREPORTE", lblTitulo.Text);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FECHA",  dtFechaInicio.Value.ToString("d") + " - " + dtFechaFinal.Value.ToString("d"));
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@USERNAME", "jumAngie");
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@EMISION", DateTime.Now.ToString("g"));
+
+            string filas = string.Empty;
+            decimal total = 0;
+            foreach (DataGridViewRow row in dgReporte.Rows)
+            {
+                filas += "<tr>";
+                filas += "<td>" + row.Cells["inve_Id"].Value.ToString() + "</td>";
+                filas += "<td>" + row.Cells["inve_Titulo"].Value.ToString() + "</td>";
+                filas += "<td>" + row.Cells["fact_NumFactura"].Value.ToString() + "</td>";
+                filas += "<td>" + row.Cells["inve_Total"].Value.ToString() + "</td>";
+                filas += "</tr>";
+                total += decimal.Parse(row.Cells["inve_Total"].Value.ToString());
+            }
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@FILAS", filas);
+            PaginaHTML_Texto = PaginaHTML_Texto.Replace("@TOTAL", total.ToString());
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                using (FileStream stream = new FileStream(savefile.FileName, FileMode.Create))
+                {
+                    //Creamos un nuevo documento y lo definimos como PDF
+                    Document pdfDoc = new Document(PageSize.A4, 25, 25, 25, 25);
+
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+                    pdfDoc.Add(new Phrase(""));
+
+                    //Agregamos la imagen del banner al documento
+                    iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(Properties.Resources.logo, System.Drawing.Imaging.ImageFormat.Png);
+                    img.ScaleToFit(100, 100);
+                    img.Alignment = iTextSharp.text.Image.UNDERLYING;
+
+                    //img.SetAbsolutePosition(10,100);
+                    img.SetAbsolutePosition(pdfDoc.LeftMargin, pdfDoc.Top - 60);
+                    pdfDoc.Add(img);
+
+
+                    //pdfDoc.Add(new Phrase("Hola Mundo"));
+                    using (StringReader sr = new StringReader(PaginaHTML_Texto))
+                    {
+                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    }
+
+                    pdfDoc.Close();
+                    stream.Close();
+                }
+
+            }
+
         }
     }
 }
