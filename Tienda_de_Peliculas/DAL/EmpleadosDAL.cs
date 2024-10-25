cdf732fb355668e6dae0ff5792218aa0d66b0c8a
C4rl0s05 <carlos.cfac32@gmail.com>
2024-10-25 00:40:56 -0600

    Listar e Insertar de empleados
    No sé porque no me funciono lo de los combobox

---
+++ Tienda_de_Peliculas/DAL/EmpleadosDAL.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tienda_de_Peliculas.Clases;
+using Tienda_de_Peliculas.View_Models;
+
+namespace Tienda_de_Peliculas.DAL
+{
+    internal class EmpleadosDAL
+    {
+        public static string CantidadDeEmpleadosRegistrados()
+        {
+            string EmpleadosRegistrados = "0";
+            var empleados = 0;
+
+            try
+            {
+                using (SqlConnection conexion = BDConexion.ObtenerConexion())
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand(ScriptsDatabase.ClientesRegistrados, conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    empleados = (int)cmd.ExecuteScalar();
+
+                    EmpleadosRegistrados = Convert.ToString(empleados);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                EmpleadosRegistrados = "Error: " + ex.Message;
+                throw;
+            }
+
+
+            return EmpleadosRegistrados;
+        }
+
+        //LISTAR
+        public static List<EmpleadosViewModel> ListarEmpleados()
+        {
+            List<EmpleadosViewModel> lista = new List<EmpleadosViewModel>();
+
+            using (SqlConnection conexion = BDConexion.ObtenerConexion())
+            {
+                conexion.Open();
+                string query = "SELECT * FROM Gral.DatosGenerales_Empleado";
+                SqlCommand comando = new SqlCommand(query, conexion);
+
+                SqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    EmpleadosViewModel empleadosView = new EmpleadosViewModel();
+                    empleadosView.NombreCompleto = reader.GetString(0);
+                    empleadosView.DNI = reader.GetString(1);
+                    empleadosView.Telefono = reader.GetString(2);
+                    empleadosView.email = reader.GetString(3);
+                    empleadosView.Pais_Descripcion = reader.GetString(4);
+                    empleadosView.Dept_Descripcion = reader.GetString(5);
+                    empleadosView.Ciud_Descripcion = reader.GetString(6);
+                    empleadosView.Direccion = reader.GetString(7);
+                    empleadosView.FechaNacimiento = reader.GetDateTime(8);
+                    empleadosView.Sexo_Descripcion = reader.GetString(9);
+                    empleadosView.Cate_Descripcion = reader.GetString(10);
+                    empleadosView.usua_Usuario = reader.GetString(11);
+                    empleadosView.Fecha_Creacion = reader.GetDateTime(12);
+
+
+                    lista.Add(empleadosView);
+                }
+
+                conexion.Close();
+                return lista;
+            }
+
+
+        }
+
+        //INSERTAR
+        public static string InsertarEmpleados(DatosGenerales DgEmpleados)
+        {
+            string Mensaje = "";
+            try
+            {
+                using (SqlConnection conexion = BDConexion.ObtenerConexion())
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("Gral.UDP_tbDatosGenerales_Insertar", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    string fechaformateada = DgEmpleados.dato_FechaNacimiento.ToString("yyyy-MM-dd");
+
+                    cmd.Parameters.AddWithValue("@dato_NombreCompleto", DgEmpleados.dato_NombreCompleto);
+                    cmd.Parameters.AddWithValue("@dato_DNI", DgEmpleados.dato_DNI);
+                    cmd.Parameters.AddWithValue("@dato_Telefono", DgEmpleados.dato_Telefono);
+                    cmd.Parameters.AddWithValue("@dato_email", DgEmpleados.dato_email);
+                    cmd.Parameters.AddWithValue("@ciud_Id", DgEmpleados.ciud_Id);
+                    cmd.Parameters.AddWithValue("@dato_Direccion", DgEmpleados.dato_Direccion);
+                    cmd.Parameters.AddWithValue("@dato_FechaNacimiento", fechaformateada);
+                    cmd.Parameters.AddWithValue("@sexo_Id", DgEmpleados.sexo_Id);
+                    cmd.Parameters.AddWithValue("@cate_Id", DgEmpleados.cate_Id);
+                    cmd.Parameters.AddWithValue("@usua_UsuarioCreacion", DgEmpleados.usua_UsuarioCreacion);
+                    cmd.Parameters.AddWithValue("@dato_FechaCreacion", DgEmpleados.dato_FechaCreacion);
+
+
+                    Mensaje = (string)cmd.ExecuteScalar();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Error: " + ex.Message;
+                throw;
+            }
+
+
+
+            return Mensaje;
+        }
+    }
+}
--- Tienda_de_Peliculas/Formularios/frmDatosGeneralesEmpleados.cs
+++ Tienda_de_Peliculas/Formularios/frmDatosGeneralesEmpleados.cs
@@ -7,11 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tienda_de_Peliculas.Clases;
+using Tienda_de_Peliculas.DAL;
 
 namespace Tienda_de_Peliculas
 {
     public partial class frmDatosGeneralesEmpleados : Form
     {
+        #region CLASES
+        PaisesDAL paises = new PaisesDAL();
+        DepartamentosDAL depto = new DepartamentosDAL();
+        CiudadesDAL ciudad = new CiudadesDAL();
+        #endregion
+        #region VARIABLES
+        private int sexo = 0;
+        private int empleado = 1;
+        #endregion
         public frmDatosGeneralesEmpleados()
         {
             InitializeComponent();
@@ -32,5 +43,137 @@ namespace Tienda_de_Peliculas
         {
             LoadTheme();
         }
+
+
+        #region CRUD
+        public void ListarEmpleados()
+        {
+            dgDatosEmpleado.DataSource = EmpleadosDAL.ListarEmpleados();
+        }
+
+        public void InsertarEmpleados()
+        {
+            if (rbF.Checked) sexo = 1;
+            if (rbM.Checked) sexo = 2;
+
+            DatosGenerales Dg = new DatosGenerales
+            {
+                dato_NombreCompleto = txbName.Text,
+                dato_DNI = txtDNI.Text,
+                dato_Telefono = txbTel.Text,
+                dato_FechaNacimiento = dtFechaNacimiento.Value,
+                sexo_Id = sexo,
+                dato_email = txbemail.Text,
+                ciud_Id = Convert.ToInt32(cbxCiudad.SelectedValue),
+                dato_Direccion = txbDirE.Text,
+                cate_Id = empleado,
+                dato_FechaCreacion = DateTime.Now,
+                usua_UsuarioCreacion = 1
+            };
+
+            string resultados = EmpleadosDAL.InsertarEmpleados(Dg);
+            MessageBox.Show(resultados, "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        #endregion
+
+        #region LLENANDO COMBOBOX
+        public void CargarPaisesCMB()
+        {
+            cbxPais.DataSource = paises.CargarPaises();
+            cbxPais.ValueMember = "pais_Id";
+            cbxPais.DisplayMember = "pais_Descripcion";
+        }
+
+        private void CargarDeptoPorPaisCMB(int pais_Id)
+        {
+            cbxDepto.DataSource = depto.CargarDepartamentosPorPais(pais_Id);
+            cbxDepto.ValueMember = "dept_Id";
+            cbxDepto.DisplayMember = "dept_Descripcion";
+        }
+        public void CargarCiudadesporDepartamentoCMB(int dept_Id)
+        {
+            cbxCiudad.DataSource = ciudad.CargarCiudadesPorDepto(dept_Id);
+            cbxCiudad.ValueMember = "ciud_Id";
+            cbxCiudad.DisplayMember = "ciud_Descripcion";
+        }
+        #endregion
+        #region VALIDACIONES Y LIMPIEZA DE CAMPOS
+
+        public void ValidandoVacios()
+        {
+            // mostrar error
+
+            //esconder error
+        }
+        public void LimpiarCampos()
+        {
+            txbName.Clear();
+            txbTel.Clear();
+            txbDirE.Clear();
+            txbemail.Clear();
+            txtDNI.Clear();
+            rbF.Checked = false;
+            rbM.Checked = false;
+            cbxCiudad.Enabled = false;
+            cbxDepto.Enabled = false;
+            cbxDepto.Text = "Seleccione un país.";
+            cbxCiudad.Text = "Seleccione un departamento.";
+            cbxPais.SelectedIndex = 0;
+            dtFechaNacimiento.Value = DateTime.Now;
+        }
+        #endregion
+
+        #region EVENTOS DE LOS ELEMENTOS DEL FORMULARIO
+        private void frmDatosGeneralesEmpleado_Load (object sender, EventArgs e)
+        {
+            LoadTheme();
+            ListarEmpleados();
+            CargarPaisesCMB();
+            cbxDepto.Text = "Seleccione un país.";
+            cbxCiudad.Text = "Seleccione un departamento.";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            InsertarEmpleados();
+            ListarEmpleados();
+            LimpiarCampos();
+
+        }
+        private void cbxPais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxPais.SelectedValue != null && cbxPais.SelectedValue is int)
+            {
+                cbxDepto.Enabled = true;
+                int pais_Id = (int)cbxPais.SelectedValue;
+                CargarDeptoPorPaisCMB(pais_Id);
+            }
+        }
+
+        private void cbxDepto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbxDepto.SelectedValue != null && cbxDepto.SelectedValue is int)
+            {
+
+                int dept_Id = (int)cbxDepto.SelectedValue;
+                if (dept_Id > 0) // verifica que sea un valor válidop
+                {
+                    cbxCiudad.Enabled = true;
+                    CargarCiudadesporDepartamentoCMB(dept_Id);
+                }
+            }
+            else
+            {
+                cbxCiudad.DataSource = null;
+                cbxCiudad.Enabled = false;
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+        #endregion
+
     }
 }
