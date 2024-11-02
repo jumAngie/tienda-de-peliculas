5b169950cd233ff96abe7bc77060db5afbf38a50
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-11-02 16:04:09 -0600

    me dio pereza bye

---
+++ Tienda_de_Peliculas/View Models/PantallasViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tienda_de_Peliculas.View_Models
+{
+    public class PantallasViewModel
+    {
+        public int pant_ID { get; set; }
+        public string pant_NombrePantalla { get; set; }
+        public string pant_RutaImagen { get; set; }
+        public string pant_NombreBoton { get; set; }
+        public int PosicionY { get; set; }
+    }
+}
